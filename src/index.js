63e5082be1c05e9333c841862175389abde341f9
rustytrooper <diansolo40@gmail.com>
2023-08-01 20:20:57 +0300

    init: done task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
+  let absolute = Math.abs(n)
+  let arrFromNum=Array.from(String(absolute))
+  let reversed = Number(arrFromNum.reverse().join(''))
+  return reversed
 }
